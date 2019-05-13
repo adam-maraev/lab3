428979eb92b43c4d6bfb951841d7bc49ee26f2df
adam-maraev <49427083+adam-maraev@users.noreply.github.com>
2019-05-13 20:46:03 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,156 @@
+﻿using IGI_3.Models;
+using IGI_3.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace IGI_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        Repository repository;
+
+        public HomeController(Repository repository)
+        {
+            this.repository = repository;
+        }
+        
+        #region Views
+        [ResponseCache(CacheProfileName = "Caching")]
+        public IActionResult Client()
+        {
+            var model = repository.GetClients();
+            return View(model);
+        }
+        [ResponseCache(CacheProfileName = "Caching")]
+        public IActionResult Furniture()
+        {
+            var model = repository.GetFurnitures();
+            return View(model);
+        }
+        [ResponseCache(CacheProfileName = "Caching")]
+        public IActionResult Order()
+        {
+            var model = repository.GetOrders();
+            return View(model);
+        }
+        [ResponseCache(CacheProfileName = "Caching")]
+        public IActionResult Worker()
+        {
+            var model = repository.GetWorkers();
+            return View(model);
+        }
+        
+        [HttpGet]
+        public ActionResult AddClient()
+        {
+            Client model = null;
+            if (HttpContext.Session.Keys.Contains("AddClient"))
+            {
+                model = JsonConvert.DeserializeObject<Client>(HttpContext.Session.GetString("AddClient"));
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddFurniture()
+        {
+            Furniture model = null;
+            if (HttpContext.Session.Keys.Contains("AddFurniture"))
+            {
+                model = JsonConvert.DeserializeObject<Furniture>(HttpContext.Session.GetString("AddFurniture"));
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddOrder()
+        {
+            var model = new OrderViewModel
+            {
+                Order = null,
+                Clients = repository.GetClients(),
+                Furnitures = repository.GetFurnitures(),
+                Workers = repository.GetWorkers()
+            };
+            if (HttpContext.Session.Keys.Contains("AddOrder"))
+            {
+                model.Order = JsonConvert.DeserializeObject<Order>(HttpContext.Session.GetString("AddOrder"));
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddWorker()
+        {
+            Worker model = null;
+            if (HttpContext.Session.Keys.Contains("AddWorker"))
+            {
+                model = JsonConvert.DeserializeObject<Worker>(HttpContext.Session.GetString("AddWorker"));
+            }
+            return View(model);
+        }
+        #endregion
+
+        #region Add functional
+        [HttpPost]
+        public ActionResult AddClient(Client client, string action)
+        {
+            if (action == "Add")
+            {
+                HttpContext.Session.Remove("AddClient");
+                repository.AddClient(client);
+            }
+            else
+            {
+                HttpContext.Session.SetString("AddClient", JsonConvert.SerializeObject(client));
+            }
+            return RedirectToAction("Client");
+        }
+        [HttpPost]
+        public ActionResult AddFurniture(Furniture furniture, string action)
+        {
+            if (action == "Add")
+            {
+                HttpContext.Session.Remove("AddFurniture");
+                repository.AddFurniture(furniture);
+            }
+            else
+            {
+                HttpContext.Session.SetString("AddFurniture", JsonConvert.SerializeObject(furniture));
+            }
+            return RedirectToAction("Furniture");
+        }
+        [HttpPost]
+        public ActionResult AddOrder(Order order, string clientName, string furnitureName, string workerName, string action)
+        {
+            order.Client = repository.GetClientByName(clientName);
+            order.Furniture = repository.GetFurnitureByName(furnitureName);
+            order.Worker = repository.GetWorkerByName(workerName);
+            if (action == "Add")
+            {
+                HttpContext.Session.Remove("AddOrder");
+                repository.AddOrder(order);
+            }
+            else
+            {
+                HttpContext.Session.SetString("AddOrder", JsonConvert.SerializeObject(order));
+            }
+
+            return RedirectToAction("Order");
+        }
+        [HttpPost]
+        public ActionResult AddWorker(Worker worker, string action)
+        {
+            if (action == "Add")
+            {
+                HttpContext.Session.Remove("AddWorker");
+                repository.AddWorker(worker);
+            }
+            else
+            {
+                HttpContext.Session.SetString("AddWorker", JsonConvert.SerializeObject(worker));
+            }
+            return RedirectToAction("Worker");
+        }
+        #endregion
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,84 @@
+﻿
+using IGI_3.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IGI_3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddTransient<Repository>();
+            services.AddMemoryCache();
+
+
+
+
+            services.AddMvc(options =>
+            {
+                options.CacheProfiles.Add("Caching", new CacheProfile
+                {
+                    Location = ResponseCacheLocation.Any,
+                    Duration = 262
+                });
+                options.CacheProfiles.Add("NoCaching", new CacheProfile
+                {
+                    Location = ResponseCacheLocation.None,
+                    NoStore = true
+                });
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+
+            app.UseSession();
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
